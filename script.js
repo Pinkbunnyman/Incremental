783c30679176bde05f43498fc94e1ce93d60bbac
James Taylor <James>
2024-07-10 14:18:47 +0100

    removed animation

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
         var bar = document.createElement("div");
         bar.id = barName + "Bar";
         bar.className = "progress-bar progress-bar-striped";
-        bar.style.transition = "40ms"
+        bar.style.transition = "none"
 
         var title = document.createElement("h2");
         title.textContent = barName;
